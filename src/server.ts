978f7b795fda799d359db4414c21d437faaf9c31
ADIsaacNascimento <104455835+ADIsaacNascimento@users.noreply.github.com>
2022-11-06 03:00:48 -0300

    Modelagem DB, run seeders, zod library to validations and UUID library

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from "fastify";
 import { PrismaClient } from "@prisma/client";
 import cors from "@fastify/cors";
+import { z } from "zod";
+import ShortUniqueId from 'short-unique-id';
 
 const prisma = new PrismaClient({
   log: ["query"],
@@ -13,7 +15,7 @@ const bootstrap = async () => {
 
   await fastify.register(cors, {
     origin: true,
-  })
+  });
 
   fastify.get("/pools/count", async () => {
     const pools = await prisma.pool.count();
@@ -21,7 +23,44 @@ const bootstrap = async () => {
     return { count: pools };
   });
 
-  await fastify.listen({ port: 3333, host: '0.0.0.0' });
+  fastify.get("/users/count", async () => {
+    const usersQuantity = await prisma.user.count();
+
+    return { usersQuantity };
+  });
+
+  fastify.get("/guesses/count", async () => {
+    const guessesQuantity = await prisma.guess.count();
+
+    return { guessesQuantity };
+  });
+
+  fastify.post("/pools", async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    });
+
+    try {
+      const { title } = createPoolBody.parse(request.body);
+
+      const generateID = new ShortUniqueId({length: 6});
+      const code = String(generateID()).toUpperCase();
+     
+      await prisma.pool.create({
+        data: {
+          title,
+          code,
+        }
+      })
+
+      return reply.status(201).send({ code });
+
+    } catch (error) {
+      return `The Title field cannot be null - ${error}`;
+    }
+  });
+
+  await fastify.listen({ port: 3333, host: "0.0.0.0" });
 };
 
 bootstrap();
